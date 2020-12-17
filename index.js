a1ca39306ca53de7a1a120d0b18c4b6f8e0a4d1b
Artsiom Biba <66476225+ArtsyBiba@users.noreply.github.com>
2020-12-17 16:12:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const autoCompleteConfig = {
 	async fetchData(searchTerm) {
 		const response = await axios.get('http://www.omdbapi.com/', {
 			params : {
-				apikey : '3644fa00',
+				apikey : '???',
 				s      : searchTerm
 			}
 		});
